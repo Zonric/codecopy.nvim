35cff909a7c75e287b4f7f7300dd828ad7804d46
William Rice <william@williamarice.com>
2025-06-14 15:00:57 -0400

    moved integration response to the integration module

--- lua/codecopy/integrations/gist.lua
+++ lua/codecopy/integrations/gist.lua
@@ -3,29 +3,45 @@ local M = {}
 ---Builds the payload
 function M.build(data)
 	local integration = data.selected_integration
-  local payload_builder = {
-    description = data.message .."\n",
-    public = integration.public or false,
-    files = {
-      [data.file.name] = {
-        content = data.codecopy,
-      }
-    }
-  }
+	local payload_builder = {
+		description = data.message .. "\n",
+		public = integration.public or false,
+		files = {
+			[data.file.name] = {
+				content = data.codecopy,
+			},
+		},
+	}
 
 	local payload = vim.fn.json_encode(payload_builder)
 
 	return {
 		cmd = {
 			"curl",
-			"-X", "POST",
-			"-H", "Accept: application/vnd.github+json",
-			"-H", "Authorization: Bearer " .. integration.token,
-			"-H", "X-GitHub-Api-Version: 2022-11-28",
-			"-d", payload,
+			"-X",
+			"POST",
+			"-H",
+			"Accept: application/vnd.github+json",
+			"-H",
+			"Authorization: Bearer " .. integration.token,
+			"-H",
+			"X-GitHub-Api-Version: 2022-11-28",
+			"-d",
+			payload,
 			"https://api.github.com/gists",
 		},
 	}
+end
+
+M.handle_response = function(data)
+	-- a successful response doesnt't have a status code in the data. but there is an id
+	-- TODO check if gist id is present
+	local id = data[5]
+	if id and string.match(id, "^[0-9]+$") then
+		vim.notify("Payload sent successfully.", vim.log.levels.INFO, { title = "CodeCopy Info:" })
+	else
+		vim.notify("Payload failed", vim.log.levels.ERROR, { title = "CodeCopy Error:" })
+	end
 end
 
 return M
--- lua/codecopy/integrations/zulip.lua
+++ lua/codecopy/integrations/zulip.lua
@@ -1,35 +1,50 @@
 local M = {}
 
 function M.build(data)
-	local options = require ("codecopy.config").options
+	local options = require("codecopy.config").options
 	local integration = data.selected_integration
 	local payload = {
 		content = "",
 	}
 	if data.message ~= nil and data.message ~= "" then
-		payload.content = "# "..data.message.."\n\n"
+		payload.content = "# " .. data.message .. "\n\n"
 	end
 	if options.code_fence then
-		payload.content = payload.content.."```"..data.file.lang.."\n"..data.codecopy.."\n```"
+		payload.content = payload.content .. "```" .. data.file.lang .. "\n" .. data.codecopy .. "\n```"
 	else
 		payload.content = data.codecopy
 	end
 	if options.include_file_path then
-		payload.content = payload.content.."\n\n*"..data.file.path.."*"
+		payload.content = payload.content .. "\n\n*" .. data.file.path .. "*"
 	end
 
 	return {
 		cmd = {
 			"curl",
-			"-X", "POST",
+			"-X",
+			"POST",
 			integration.url,
-			"-u", integration.username..":"..integration.token,
-			"--data-urlencode", "type=stream",
-			"--data-urlencode", "to="..integration.channel,
-			"--data-urlencode", "topic="..integration.topic,
-			"--data-urlencode", "content="..payload.content,
+			"-u",
+			integration.username .. ":" .. integration.token,
+			"--data-urlencode",
+			"type=stream",
+			"--data-urlencode",
+			"to=" .. integration.channel,
+			"--data-urlencode",
+			"topic=" .. integration.topic,
+			"--data-urlencode",
+			"content=" .. payload.content,
 		},
 	}
+end
+
+function M.handle_response(data)
+	local response = vim.fn.json_decode(data[1])
+	if response.result == "success" then
+		vim.notify("Payload sent successfully.", vim.log.levels.INFO, { title = "CodeCopy Info:" })
+	else
+		vim.notify("Payload failed", vim.log.levels.ERROR, { title = "CodeCopy Error:" })
+	end
 end
 
 return M
