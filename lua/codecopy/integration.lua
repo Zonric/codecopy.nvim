f625f51cb2a751efaf2de382996623f9f739cd9a
William Rice <william@williamarice.com>
2025-06-14 01:43:20 -0400

    handle curl standard out response

--- lua/codecopy/integration.lua
+++ lua/codecopy/integration.lua
@@ -7,16 +7,18 @@ function M.dispatch()
 	local options = require("codecopy.config").options
 	local state = require("codecopy.state")
 	local integration = state.data.selected_integration
-	if integration.target == "clipboard" then return end
+	if integration.target == "clipboard" then
+		return
+	end
 	local module_name = ""
 	if options.env.enabled then
-		module_name = "codecopy.integrations."..integration.target
+		module_name = "codecopy.integrations." .. integration.target
 	else
 		module_name = "codecopy.integrations.default"
 	end
 	local ok, integration_module = pcall(require, module_name)
 	if not ok then
-		vim.notify("Missing integration: ".. module_name, vim.log.levels.ERROR, { title = "CodeCopy Error:" })
+		vim.notify("Missing integration: " .. module_name, vim.log.levels.ERROR, { title = "CodeCopy Error:" })
 		if options.debug then
 			vim.notify(vim.inspect(integration_module))
 		end
@@ -30,12 +32,18 @@ function M.dispatch()
 		on_exit = function(_, code)
 			if code ~= 0 then
 				vim.notify("curl exited with code: " .. code, vim.log.levels.ERROR, { title = "CodeCopy Error:" })
-			else
-				vim.notify("Payload sent successfully.", vim.log.levels.INFO, { title = "CodeCopy Info:" })
 			end
 		end,
+		on_stdout = function(_, data)
+			local d = vim.fn.json_decode(data[1])
+			if d.result == "success" then
+				vim.notify("Payload sent successfully.", vim.log.levels.INFO, { title = "CodeCopy Info:" })
+			else
+				vim.notify("Payload failed", vim.log.levels.ERROR, { title = "CodeCopy Error:" })
+			end
+		end,
+		stdout_buffered = true,
 	})
 end
 
 return M
-
