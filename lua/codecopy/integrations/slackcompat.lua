5c7d1fd99e5265d9f87fdf003502b6f8e4282803
Zonric <zonric42@gmail.com>
2025-06-14 22:33:48 -0400

    Complete Integrations
    Finishing Touches to the Integrations.

--- lua/codecopy/integrations/slackcompat.lua
+++ lua/codecopy/integrations/slackcompat.lua
@@ -2,20 +2,20 @@
 local M = {}
 
 M.get_payload = function(data)
-	local options = require ("codecopy.config").options
+	local options = require("codecopy.config").options
 	local integration = data.selected_integration
 	local message = ""
 	if data.message ~= nil and data.message ~= "" then
-		message = "# "..data.message.."\n\n"
+		message = "# " .. data.message .. "\n\n"
 	end
 	local codecopy = ""
 	if options.code_fence then
-		codecopy = "```"..data.file.lang.."\n"..data.codecopy.."\n```"
+		codecopy = "```" .. data.file.lang .. "\n" .. data.codecopy .. "\n```"
 	else
 		codecopy = data.codecopy
 	end
 	if options.include_file_path then
-		codecopy = codecopy .. "\n\n*"..data.file.path.."*"
+		codecopy = codecopy .. "\n\n*" .. data.file.path .. "*"
 	end
 	local payload = vim.fn.json_encode({
 		blocks = {
@@ -25,7 +25,8 @@ M.get_payload = function(data)
 					type = "mrkdwn",
 					text = message,
 				},
-			},{
+			},
+			{
 				type = "section",
 				text = {
 					type = "mrkdwn",
@@ -36,15 +37,8 @@ M.get_payload = function(data)
 	})
 
 	return {
-		cmd = {
-			"curl",
-			"-X", "POST",
-			"-H", "Content-Type: application/json",
-			"-d", payload,
-			integration.url
-		}
+		cmd = { "curl", "-X", "POST", "-H", "Content-Type: application/json", "-d", payload, integration.url },
 	}
-
 end
 
 return M
