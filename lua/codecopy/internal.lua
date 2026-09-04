5f3140084a0ab245f1edbcaa6501c60ca81ea573
Zonric <zonric@nullknights.com>
2026-09-04 04:51:23 -0400

    fix: crash/error when env is enabled but file doesnt exist.

--- lua/codecopy/internal.lua
+++ lua/codecopy/internal.lua
@@ -1,10 +1,30 @@
 local M = {}
+local options = require("codecopy.config").options
+
+local function fileExists(filepath)
+	local file = io.open(filepath, "r")
+	if file then
+		file:close()
+		return true
+	else
+		return false
+	end
+end
 
 function M.import_env(filepath)
-	local lines = vim.fn.readfile(vim.fn.expand(filepath))
-	local json_str = table.concat(lines, "\n")
-	local env = vim.json.decode(json_str)
-
+	if not options.env.enabled then
+		return
+	end
+	filepath = vim.fn.expand(filepath)
+	local env = nil
+	if fileExists(filepath) then
+		local lines = vim.fn.readfile(filepath)
+		local json_str = table.concat(lines, "\n")
+		env = vim.json.decode(json_str)
+	else
+		vim.notify("Env is enabled but " .. filepath .. " file doesnt exist.", vim.log.levels.WARN, { title = "CodeCopy Config Warning:" })
+		options.env.enabled = false
+	end
 	return env
 end
 
